56c53075d7f90a1394d9d9e4e9d92f452ddf2837
rayanthoney <rmaxey911@gmail.com>
2022-10-26 20:25:21 -0600

    CodingPhase completed Hero section

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,81 @@
+<div class="hero">
+    <div class="hero__images">
+        <div class="hero__image">
+            <img src="https://images.familyhomeplans.com/plans/44207/44207-b600.jpg" class="hero__image-img">
+            <!-- Hero Info Section -->
+            <div class="hero__info">
+                <span class="hero__subtitle">
+                    Residential
+                </span>
+                <span class="hero__title">
+                    234 Grand Ave
+                </span>
+                <a href="#" class="hero__info-link">
+                    See Property
+                </a>
+            </div>
+        </div>
+        <div class="hero__image">
+            <img src="https://images.familyhomeplans.com/plans/52957/52957-b600.jpg" class="hero__image-img">
+            <!-- Hero Info Section -->
+            <div class="hero__info">
+                <span class="hero__subtitle">
+                    Residential
+                </span>
+                <span class="hero__title">
+                    234 Grand Ave
+                </span>
+                <a href="#" class="hero__info-link">
+                    See Property
+                </a>
+            </div>
+        </div>
+        <div class="hero__image">
+            <img src="https://i.ytimg.com/vi/bBQp3mPnKSs/maxresdefault.jpg" class="hero__image-img">
+            <!-- Hero Info Section -->
+            <div class="hero__info">
+                <span class="hero__subtitle">
+                    Residential
+                </span>
+                <span class="hero__title">
+                    234 Grand Ave
+                </span>
+                <a href="#" class="hero__info-link">
+                    See Property
+                </a>
+            </div>
+        </div>
+            <!-- Hero Slider Section -->
+        <div class="hero__indicator">
+            <div class="hero__indicator-item">
+                <span class="hero__indicator-number">
+                    01
+                </span>
+                <span class="hero__indicator-text">
+                    Summer Villa
+                </span>
+            </div>
+            <div class="hero__indicator-item">
+                <span class="hero__indicator-number">
+                    02
+                </span>
+                <span class="hero__indicator-text">
+                    Summer Villa
+                </span>
+            </div>
+            <div class="hero__indicator-item">
+                <span class="hero__indicator-number">
+                    03
+                </span>
+                <span class="hero__indicator-text">
+                    Summer Villa
+                </span>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,39 +1,5 @@
   @extends('layouts.main')
 
   @section('content')
-   <div class="hero">
-      <div class="hero__info">
-        <span class="hero__subtitle">
-           Residential
-        </span>
-        <span class="hero__title">
-           Residential
-        </span>
-        <span class="hero__info-link">
-           Residential
-        </span>
-      </div>
-      <div class="hero__indicator">
-        <div class="hero__indicator-item">
-          <span class="hero__indicator-number">
-            01
-          </span>
-          <span class="hero__indicator-text">
-            Summer Villa
-          </span>
-          <span class="hero__indicator-number">
-            01
-          </span>
-        </div>
-        <div class="hero__indicator-item">
-          <span class="hero__indicator-number">
-            01
-          </span>
-          <span class="hero__indicator-text">
-            Summer Villa
-          </span>
-        </div>
-        <div class="hero__indicator-item"></div>
-      </div>
-  </div>
+   @include('components.hero')
   @endsection
