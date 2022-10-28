d7d5636c95927c978e4aa07c71b6eab2cb0b0bab
rayanthoney <rmaxey911@gmail.com>
2022-10-28 06:19:55 -0600

    Codingphase create listings page

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="listing-page">
-    <div class="listing-city">
+<div class="listings-page">
+    <div class="listings-city">
         <img class="listings-city__img" src="https://myersandmyersrealestate.com/wp-content/uploads/2017/12/Home-Buyers-In-Albuquerque-NM.jpg">
-        <h1>Albuquerque</h1>
+        <h1 class="listings-city__title">Albuquerque</h1>
+    </div>
+    <div class="listings-filter">
+        <div class="filter-option">
+            Any Price
+        </div>
+        <div class="filter-option">
+            All Beds
+        </div>
+        <div class="filter-option">
+            Hometype
+        </div>
+        <div class="filter-option">
+            More
+        </div>
     </div>
 </div>
-    Testing
 @endsection
