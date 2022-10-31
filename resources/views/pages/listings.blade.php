a3a3af472ec003e58b20aa84a88ae79751f21163
rayanthoney <rmaxey911@gmail.com>
2022-10-31 06:43:12 -0600

    codingphase completed the listings html page

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
+{{-- Beginning of Listings Page --}}
 <div class="listings-page">
+    {{-- Beginning of Listings City --}}
     <div class="listings-city">
-        <img class="listings-city__img" src="https://myersandmyersrealestate.com/wp-content/uploads/2017/12/Home-Buyers-In-Albuquerque-NM.jpg">
+        <img class="lisitings-city__img" src="https://myersandmyersrealestate.com/wp-content/uploads/2017/12/Home-Buyers-In-Albuquerque-NM.jpg">
         <h1 class="listings-city__title">Albuquerque</h1>
     </div>
+     {{-- Ending of Listings City --}}
+
+     {{-- Beginning of Listings Filter --}}
     <div class="listings-filter">
         <div class="listings-filter__wrapper">
             <div class="listings-filter__option">
@@ -15,7 +20,7 @@
                 All Beds <i class="fa-solid fa-caret-down"></i>
             </div>
             <div class="listings-filter__option">
-                Hometype <i class="fa-solid fa-caret-down"></i>
+                Home type <i class="fa-solid fa-caret-down"></i>
             </div>
             <div class="listings-filter__option">
                 More <i class="fa-solid fa-caret-down"></i>
@@ -25,12 +30,39 @@
             </div>
         </div>
     </div>
-    <div class="listings--properties">
-        <div class="col-md-4">
-            <div class="listings-properties__item">
-                <img src="https://myersandmyersrealestate.com/wp-content/uploads/2017/12/Home-Buyers-In-Albuquerque-NM.jpg" alt="">
-            </div>
+    {{-- Ending of Listings Filter --}}
+
+    {{-- Beginning of Listings Properties --}}
+    <div class="listings-properties">
+        <div class="container">
+          <div class="row">
+            @for ($i = 1; $i <= 12; $i++)
+              <div class="col-sm-6 col-lg-4 col-xl-3">
+                <a href="/listing/2123-grand-ave-miami-fl-33456/1" class="listings-properties__item">
+                  <img src="https://myersandmyersrealestate.com/wp-content/uploads/2017/12/Home-Buyers-In-Albuquerque-NM.jpg">
+                  <div class="listings-properties__saved ">
+                    <i class="fa-solid fa-heart"></i>
+                  </div>
+    
+                  <span class="listings-properties__item-price">$250,000</span>
+                  <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler"></i> 2440 SQFT</span>
+                  <span class="listings-properties__item-address">
+                    308 Negra Arroyo,<br>
+                    Albuquerque, NM 87109
+                  </span>
+                  <div class="listings-properties__item-line"></div>
+                  <span class="listings-properties__item-owner">
+                    Bryant Realty
+                  </span>
+                </a>
+              </div>
+            @endfor
+            
+            
+            
+          </div>
         </div>
-    </div>
+    {{-- Ending of Listings Properties --}}
 </div>
+{{-- Ending of Listings Page --}}
 @endsection
