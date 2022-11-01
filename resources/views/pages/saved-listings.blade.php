c1e227587d6b391386a57ecde1bccaadcb61463a
rayanthoney <rmaxey911@gmail.com>
2022-11-01 09:27:09 -0600

    codingohase saved listings added

---
+++ resources/views/pages/saved-listings.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.account')
+
+@section('content')
+    {{-- Beginning of Listings Properties --}}
+    <div class="listings-properties">
+      
+        <div class="row">
+          @for ($i = 1; $i <= 12; $i++)
+            <div class="col-sm-12 col-lg-4 col-xl-4">
+              <a href="/listing/2123-grand-ave-miami-fl-33456/1" class="listings-properties__item">
+                <img src="https://myersandmyersrealestate.com/wp-content/uploads/2017/12/Home-Buyers-In-Albuquerque-NM.jpg">
+                <div class="listings-properties__saved ">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+  
+                <span class="listings-properties__item-price">$250,000</span>
+                <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler"></i> 2440 SQFT</span>
+                <span class="listings-properties__item-address">
+                  308 Negra Arroyo,<br>
+                  Albuquerque, NM 87109
+                </span>
+                <div class="listings-properties__item-line"></div>
+                <span class="listings-properties__item-owner">
+                  Bryant Realty
+                </span>
+              </a>
+            </div>
+          @endfor
+          
+          
+          
+        
+  </div>
+  {{-- Ending of Listings Properties --}}
+@endsection
