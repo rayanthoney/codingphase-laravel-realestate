43aae28e389b6ee49074b9e3a6b61395cad38340
rayanthoney <rmaxey911@gmail.com>
2022-11-07 21:16:08 -0700

    codingphase added header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+{{-- Beginning of Header Page --}}
+<header class="header">
+    <div class="container">
+        <a href="/" class="header__logo">
+            Travel Nurse Realty
+        </a>
+        <div class="menu">
+            
+            <a  href="/house/for_sale/albuquerque" class="header__menu-link">Listings</a>
+            <a  href="#" class="header__menu-link">Property</a>
+            <a  href="#" class="header__menu-link">Pages</a>
+        </div>
+        <div class="header__account">
+            <a href="/account/saved" class="header__account-link"><i class="fa-solid fa-heart"></i></a>
+            <div class="header__account-link"><i class="fa-solid fa-user"></i></div>
+        </div>
+    </div>
+</header>
+{{-- End of Header Page --}}
